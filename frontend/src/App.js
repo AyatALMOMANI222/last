9e1e24dbf4240a7f85f13615725fe437c83105e0
ayat <almomanii.ayat@gmail.com>
2024-10-11 13:39:39 +0300

    edit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import eventEmitter from "./common/eventEmitter"; 
 import LoginPage from "./pages/login";
 import RegisterPage from "./pages/register";
 import HomePage from "./pages/home";
@@ -19,9 +20,11 @@ import Reservation from "./components/Reservation";
 import CreateTrip from "./components/TripComponents/AddTrip";
 import ViewTrip from "./components/TripComponents/viewTrips";
 import UsersList from "./components/UserList";
+import Loader from "./CoreComponent/Loader";
 
 const App = () => {
   const location = useLocation();
+  const [showLoader, setShowLoader] = useState(false);
 
   // Define the routes where the NavBar should be displayed
   const navBarRoutes = [
@@ -35,19 +38,33 @@ const App = () => {
     "/conferences",
     "/exhibitions",
     "/reservation/form",
-   "/create/trip",
-   "/user"
+    "/create/trip",
+    "/user"
   ];
+
+  useEffect(() => {
+    const showLoaderListener = () => setShowLoader(true);
+    const hideLoaderListener = () => setShowLoader(false);
+
+    eventEmitter.on('showLoader', showLoaderListener);
+    eventEmitter.on('hideLoader', hideLoaderListener);
+
+
+    return () => {
+      eventEmitter.off('showLoader', showLoaderListener);
+      eventEmitter.off('hideLoader', hideLoaderListener);
+    };
+  }, []);
 
   return (
     <div id="main" className="main">
       <ToastContainer />
+      <Loader show={showLoader} />
       {/* Conditionally render the NavBar based on the current route */}
       {navBarRoutes.includes(location.pathname) && <NavBar />}
       <Routes className="main">
         <Route path="/exhibitions" element={<Exhibitions />} />
         <Route path="/reservation/form" element={<Reservation />} />
-
         <Route path="/conferences" element={<ConferencesAdmin />} />
         <Route path="/flights" element={<FlightFormAdmin />} />
         <Route path="/flight/form" element={<FlightForm />} />
@@ -59,11 +76,10 @@ const App = () => {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/registertype" element={<RegisterType />} />
         <Route path="/create/trip" element={<ViewTrip />} />
-        {/* <Route path="/trip/:tripId" element={<EditTrip/>} /> */}
-        <Route path="/user" element={<UsersList/>} />
-
+        <Route path="/user" element={<UsersList />} />
       </Routes>
     </div>
   );
 };
+
 export default App;
